9b3e1432039aaaa3281c74a4f251158eff4b4ad6
himanshu dhaka <1415himanshu1415@gmail.com>
2020-11-05 14:46:33 +0530

    First Commit

---
+++ Controllers/DemandController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PharmacyMedicineSupplyPortal.Models;
+using PharmacyMedicineSupplyPortal.Repository;
+
+namespace PharmacyMedicineSupplyPortal.Controllers
+{
+    public class DemandController : Controller
+    {
+        private IDemands repo;
+        private ISupplies supplyrepo;
+        public DemandController(IDemands repo,ISupplies supplyrepo)
+        {
+            this.repo = repo;
+            this.supplyrepo = supplyrepo;
+        }
+
+        [HttpPost]
+        public IActionResult Add(MedicineDemand meds)
+        {
+           // string s = meds.Medicine + meds.Demand.ToString();
+            Demands newdemand = new Demands() {
+                Medicine=meds.Medicine,
+                Demand = meds.Demand
+            };
+            int res = repo.AddDemand(newdemand);
+            if(res>0)
+            {
+               return RedirectToAction("AddSupply",newdemand);  
+            }
+            return RedirectToAction("Index","Home");
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddSupply(Demands med)
+        {
+
+            var distributionOfStock = new List<PharmacyMedicineSupply>();
+
+            using (var httpclient = new HttpClient())
+            {
+                httpclient.BaseAddress = new Uri("https://localhost:44358/");
+                HttpResponseMessage res = await httpclient.GetAsync("api/MedicineSupply/GetSupplies/"+med.Medicine+"/"+med.Demand);
+                if (res.IsSuccessStatusCode)
+                {
+                    var result = res.Content.ReadAsStringAsync().Result;
+                    distributionOfStock = JsonConvert.DeserializeObject<List<PharmacyMedicineSupply>>(result);
+                }
+            }
+            if(distributionOfStock.Count==0)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            foreach(var supply in distributionOfStock)
+            {
+                supplyrepo.AddSupply(new Supplies { PharmacyName = supply.PharmacyName, MedicineName = supply.MedicineName, SupplyCount = supply.SupplyCount });
+            }
+            return View(distributionOfStock);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PharmacyMedicineSupplyPortal.Models;
+
+namespace PharmacyMedicineSupplyPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var stock = new List<MedicineStock>();
+
+            using (var httpclient = new HttpClient())
+            {
+                httpclient.BaseAddress = new Uri("https://localhost:44366/");
+                HttpResponseMessage res = await httpclient.GetAsync("MedicineStockInformation");
+                if (res.IsSuccessStatusCode)
+                {
+                    var result = res.Content.ReadAsStringAsync().Result;
+                    stock = JsonConvert.DeserializeObject<List<MedicineStock>>(result);
+                }
+            }
+
+            var list = new List<MedicineDemand>();
+            foreach(var med in stock)
+            {
+                list.Add(new MedicineDemand { Medicine=med.Name,Demand=0});
+            }
+            ViewBag.Demands = list;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
